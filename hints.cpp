e8c51328c4d08f8992d3ae850dc5a3e468d43b41
Schatten <sv89@tut.by>
2026-04-11 21:01:55 +0400

    Update hint

--- hints.cpp
+++ hints.cpp
@@ -4,7 +4,7 @@ class SCH_magazinesReloading
 	{
 		displayName = "$STR_SCH_magazinesReloading_hint_title";
 		description = "$STR_SCH_magazinesReloading_hint_description";
-		image = "\a3\ui_f\data\gui\cfg\hints\Miss_icon_ca.paa";
+		image = "\a3\ui_f\data\gui\cfg\hints\Reload_ca.paa";
 		dlc = -1;
 	};
 };
